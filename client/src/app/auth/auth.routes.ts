470e23bebd84e91dd4c0b69e0db4220f868367d5
GavriloaiaMircea <132777368+GavriloaiaMircea@users.noreply.github.com>
2025-03-27 21:26:47 +0200

    Revert "Added user profile page (#4)"
    This reverts commit 6bffc2944b3868a2b870dffb0d948e4fba765e35.

--- client/src/app/auth/auth.routes.ts
+++ client/src/app/auth/auth.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
 
 export const AUTH_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: RegisterComponent }
 ];
